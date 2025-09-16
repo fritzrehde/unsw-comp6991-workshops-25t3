1ed200af5035f341dd81026611289f21e51436d7
fritzrehde <fritzrehde.work@gmail.com>
2025-09-17 09:52:30 +1000

    ws01: added demo prep

---
+++ ws01/demo-prep/src/main.rs
@@ -0,0 +1,17 @@
+// to cover:
+
+// ownership rules:
+// - Each value in Rust has **one** owner.
+// - There can only be one owner at a time.
+// - When the owner goes out of scope, the value will be dropped.
+// use after free, double free, dangling pointers (next week), thread safety
+
+// immutable vs mutable
+
+// how can we simulate referencing objects temporarily (both immut and mut) without proper rust references?
+
+// enums, pattern matching, errors as values, unwrapping
+
+fn main() {
+    println!("Hello, world!");
+}
