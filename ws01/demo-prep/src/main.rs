d5b84df1c7d0c4f67d14507c9c60c8a3a56c84de
fritzrehde <fritzrehde.work@gmail.com>
2025-09-17 10:09:05 +1000

    ws01: added more demo prep notes

--- ws01/demo-prep/src/main.rs
+++ ws01/demo-prep/src/main.rs
@@ -1,5 +1,7 @@
 // to cover:
 
+// all builds stored in one dir (easy to delete): `export CARGO_TARGET_DIR="$HOME/cargo-targets"`
+
 // ownership rules:
 // - Each value in Rust has **one** owner.
 // - There can only be one owner at a time.
@@ -10,7 +12,15 @@
 
 // how can we simulate referencing objects temporarily (both immut and mut) without proper rust references?
 
-// enums, pattern matching, errors as values, unwrapping
+// structs, code blocks, enums, pattern matching, errors as values, unwrapping
+
+// () unit type (and value), demo match returning () or {}
+
+// if, match are expressions (return value)
+
+// ranges
+
+// &str vs String
 
 fn main() {
     println!("Hello, world!");
